819ab89d6e1665bfe3fbc124e34da10b8d2c9c98
Toni Henkel <tonihenkel@outlook.de>
2026-06-12 21:17:29 +0200

    Added config.php

---
+++ htdocs/execute/config.php
@@ -0,0 +1,55 @@
+<?php
+
+// MySQL Datenbank Zugriff
+$dbHost = "127.0.0.1";
+$dbName = "flight_network";
+$dbUser = "Administrator";
+$dbPass = "Yakus@nTheGhostRid3r";
+
+// Zeitzonen-Einstellungen
+$defaultTimezone = "UTC";
+date_default_timezone_set($defaultTimezone);
+
+// Minimale OP Permission, um unsichtbar sein zu können
+$minimumInvisibleOpPermission = 3;
+
+// Anzeige der Ratings überall
+$showRatings = true;
+
+// Browser-Titelname
+$projectName = "Flight Radar Sim Project";
+
+// Download Bereich
+$pluginDownloadEnabled = false;
+
+// Echter Dateipfad auf dem Server
+$pluginDownloadPath =
+    "C:/xampp/htdocs/_downloads_/FlightRadarPlugin_latest.zip";
+
+// URL für den Download Button
+$pluginDownloadUrl =
+    "execute/download_plugin.php";
+
+// Name der Download-Datei
+$pluginDownloadName =
+    "FlightRadarPlugin_latest.zip";
+
+// Rechtliche Angaben
+
+$companyName =
+    "Flight Radar Sim Project";
+
+$companyOwner =
+    "Toni Henkel";
+
+$companyAddress =
+    "Solinger Straße 5";
+
+$companyZipCity =
+    "08280 Aue";
+
+$companyCountry =
+    "Deutschland";
+
+$companyEmail =
+    "tonihenkel@mein-postbote.de";
